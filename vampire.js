233dbce5656d359abe69d862b94a81ceedf7b67b
Zoey Farago <zf.zoodle@gmail.com>
2021-01-22 22:02:25 +0000

    Intro to trees Vampr

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let currentPos = this;
+    let count = 0;
+    
+    while(currentPos.creator) {
+      currentPos = currentPos.creator;
+      count++;
+    }
+    return count; 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,7 +44,32 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    // let currentThis = this;
+    // let currentVamp = vampire;
+    // console.log(currentThis.numberOfVampiresFromOriginal);
+    // console.log(currentVamp.numberOfVampiresFromOriginal);
+    
+    // //if either is root then return root
+    // if (!vampire.creator || !this.creator) {
+    //   return this;
+    // }
 
+    // //if distance from root isn't equal make it equal 
+    // while (currentVamp.numberOfVampiresFromOriginal !== currentThis.numberOfVampiresFromOriginal) {
+    //   if (currentVamp.numberOfVampiresFromOriginal < currentThis.numberOfVampiresFromOriginal) {
+    //     currentVamp = currentVamp.creator;
+    //   } else {
+    //     currentThis = currentThis.creator;
+    //   }
+    // }
+
+    // if (currentThis.creator) {
+    //   if (currentVamp.creator === currentThis.creator) {
+    //     return currentVamp === currentThis ? currentThis : currentThis.creator;
+    //   } 
+    // } else {
+      
+    // }
   }
 }
 
